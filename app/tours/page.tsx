e82be878f363ca51f7cad2575c19c0975334c781
António Barbosa <antonio.barbosa@ignit.tech>
2024-08-16 16:20:33 +0100

    secret folders

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react'
+import Image from 'next/image'
 
 
 const url = 'https://www.course-api.com/react-tours-project';
@@ -24,12 +25,19 @@ export default async function ToursPage() {
   return (
     <section>
         <h1 className='text-3xl mb-4'>Tours</h1>
+        <div className='grid md:grid-cols-2 gap-8'>
+
+        
         {data.map((tour) => {
             return(
             <Link href={`/tours/${tour.id}`} key={tour.id} className='hover:text-blue-500'>
+                <div className='relative h-48 mb-2'>
+                    <Image src={tour.image} alt={tour.name} fill sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw' priority className='object-cover rounded'/> 
+                </div>
                 <h2>{tour.name}</h2>
             </Link>
         )})}
+        </div>
     </section>
   )
 }
