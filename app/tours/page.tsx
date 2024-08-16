2d62e8521f7079b97386d6268f5c768194755d07
António Barbosa <antonio.barbosa@ignit.tech>
2024-08-16 14:58:43 +0100

    added nested layouts

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 
@@ -24,8 +25,11 @@ export default async function ToursPage() {
     <section>
         <h1 className='text-3xl mb-4'>Tours</h1>
         {data.map((tour) => {
-            return <h2 key={tour.id}>{tour.name}</h2>
-        })}
+            return(
+            <Link href={`/tours/${tour.id}`} key={tour.id} className='hover:text-blue-500'>
+                <h2>{tour.name}</h2>
+            </Link>
+        )})}
     </section>
   )
 }
