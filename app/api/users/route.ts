7269435325fb0ee7b42117977dcc41193ad6040a
António Barbosa <antonio.barbosa@ignit.tech>
2024-08-22 16:22:57 +0100

    finished tutorial

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,20 @@
 import { fetchUsers, saveUser } from "@/utils/actions";
+import exp from "constants";
+import { NextRequest, NextResponse } from "next/server";
 
 
-export const GET= async (req:any, res:any) => {
+
+
+export const GET = async (req:NextRequest, res:NextResponse) => {
+    console.log(req.url);
+    console.log(req.nextUrl.searchParams.get('id'));
     const users = await fetchUsers();
-    return Response.json({users});
+    return NextResponse.redirect(new URL('/', req.url));
+}
+
+export const POST = async (req:NextRequest, res:NextResponse) => {
+    const user = await req.json();
+    const newUser = {id:Date.now().toString(), ...user};
+    await saveUser(newUser);
+    return Response.json({message:'User created successfully'});
 }
---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+
+export function middleware(request:Request) {
+    return NextResponse.redirect(new URL('/', request.url));
+}
+
+export const config = {
+    matcher: ['/about/:path*', '/tours/:path*'],
+};
+
